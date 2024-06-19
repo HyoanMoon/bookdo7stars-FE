e88346ec7e0803a371cef1442884973bd66eea9e
hunjoonrhee <hunjoonrhee90@gmail.com>
2024-06-19 22:41:02 +0200

    책 불러오기

--- src/components/context/bookStore/bookStoreThunk.js
+++ src/components/context/bookStore/bookStoreThunk.js
@@ -1,24 +1,17 @@
 import { messages } from './bookStore.messages';
-
-const backendURL = process.env.NODE_ENV === 'production' ? process.env.REACT_APP_BACKEND_PROXY : process.env.REACT_APP_LOCAL_BACKEND;
+import api from '../../../utils/api';
 
 export const fetchBooksThunk = async (query, { rejectWithValue }) => {
   try {
-    // const response = await api.get('/book');
-    const response = await fetch(`${backendURL}/api/book?${query}`, {
-      method: 'GET',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      credentials: 'include',
+    const response = await api.get('/book', {
+      params: { ...query },
     });
-    if (response.status !== 200) {
-      const errorMessage = await response.text(); // 응답 메시지를 확인합니다.
-      return rejectWithValue(messages.GET_ALL_BOOKS_ERROR.message || errorMessage);
-    }
 
-    const data = await response.json();
-    return data;
+    console.log(response.data);
+    if (response.status !== 200) {
+      return rejectWithValue(messages.GET_ALL_BOOKS_ERROR.message);
+    }
+    return response.data.data;
   } catch (err) {
     console.error(err);
   }
--- src/provider/store/store.js
+++ src/provider/store/store.js
@@ -9,4 +9,17 @@ export const setupStore = (preloadedState) =>
   configureStore({
     reducer: rootReducer,
     preloadedState,
+    // SerializableStateInvariantMiddleware 경고 해결
+    // Redux 상태나 액션이 직렬화할 수 있는지 검사하는 미들웨어로 인해 발생
+    middleware: (getDefaultMiddleware) =>
+      getDefaultMiddleware({
+        serializableCheck: {
+          // Ignore these field paths in all actions
+          ignoredActionPaths: ['meta.arg', 'payload.timestamp'],
+          // Ignore these paths in the state
+          ignoredPaths: ['items.dates'],
+          // Optionally set a warning threshold
+          warnAfter: 100, // Increase the threshold
+        },
+      }),
   });
