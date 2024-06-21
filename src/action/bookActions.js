aafec7b3e6aa4f12eca3ab3d863e9ed4d6bca83d
MayHyeyeonKim <hkim7963@gmail.com>
2024-06-21 02:57:55 -0700

    feat: bookDetail  컨플릭나면입맛대로하십쇼:)

--- src/action/bookActions.js
+++ src/action/bookActions.js
@@ -1,6 +1,9 @@
 import api from '../utils/api';
 import * as types from '../constants/book.constants';
 import { BOOK_GET_BY_GROUP_REQUEST } from '../constants/book.constants';
+import { Response } from '../../node_modules/whatwg-fetch/fetch';
+import { type } from '@testing-library/user-event/dist/type';
+import { commonUiActions } from './commonUiAction';
 
 // 도서 정보 불러오기.
 const getBookList = (query) => async (dispatch) => {
@@ -31,7 +34,19 @@ const getBooksByQueryType = (query, queryType) => async (dispatch) => {
 };
 
 // 도서 디테일 불러오기.
-const getBookDetail = () => async (dispatch) => {};
+const getBookDetail = (id) => async (dispatch) => {
+  try {
+    dispatch({ type: types.GET_BOOK_DETAIL_REQUEST });
+    console.log('[북액션디테일] 들어온id: ', id);
+    const response = await api.get(`book/detail/${id}`);
+    console.log('[북액션디테일] response받은것: ', response);
+    if (response.status !== 200) throw new Error(response.error);
+    dispatch({ type: types.GET_BOOK_DETAIL_SUCCESS, payload: response.data });
+  } catch (error) {
+    dispatch({ type: types.GET_BOOK_DETAIL_FAIL, payload: error.error });
+    dispatch(commonUiActions.showToastMessage(error.error, 'error'));
+  }
+};
 
 // 도서 등록.
 const createBook = () => async (dispatch) => {};
