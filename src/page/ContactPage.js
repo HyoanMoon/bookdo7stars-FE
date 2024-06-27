eb390335464627e637d2903e58b7b6a706f459d0
MayHyeyeonKim <hkim7963@gmail.com>
2024-06-27 06:50:36 -0700

    added: Contact페이지에 사이드바 빼고 중앙정렬

--- src/page/ContactPage.js
+++ src/page/ContactPage.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { styled } from '@mui/material/styles';
-import { Paper, Grid, Box } from '@mui/material';
+import { Container, Paper, Grid, Box } from '@mui/material';
 import QnaTop10 from '../components/ContactPage/QnaTop10';
 import InquiryForm from '../components/ContactPage/InquiryForm';
 import Notice from '../components/ContactPage/Notice';
@@ -39,40 +39,42 @@ const ContactPage = () => {
   };
 
   return (
-    <Box sx={{ display: 'flex' }}>
-      <Sidebar />
-      <OuterContainer sx={{ width: '85%', margin: 'auto', marginTop: 5, marginBottom: 5 }}>
-        <Grid container rowSpacing={2} columnSpacing={{ xs: 2, sm: 2, md: 3 }}>
-          <Grid item xs={12} id="faq">
-            <Item>
-              <QnaTop10 />
-            </Item>
+    <Container>
+      <Box sx={{ display: 'flex' }}>
+        {/* <Sidebar /> */}
+        <OuterContainer sx={{ width: '85%', margin: 'auto', marginTop: 5, marginBottom: 5 }}>
+          <Grid container rowSpacing={2} columnSpacing={{ xs: 2, sm: 2, md: 3 }}>
+            <Grid item xs={12} id="faq">
+              <Item>
+                <QnaTop10 />
+              </Item>
+            </Grid>
+            <Grid item xs={12} id="inquiry">
+              <Item>
+                <InquiryForm />
+              </Item>
+            </Grid>
+            <Grid item xs={12}>
+              <Item>
+                <Notice />
+              </Item>
+            </Grid>
+            <Grid item xs={12}>
+              <Item>
+                <ServicesSection />
+              </Item>
+            </Grid>
+            <Grid item xs={12}>
+              <Item>
+                <ContactSection />
+              </Item>
+            </Grid>
           </Grid>
-          <Grid item xs={12} id="inquiry">
-            <Item>
-              <InquiryForm />
-            </Item>
-          </Grid>
-          <Grid item xs={12}>
-            <Item>
-              <Notice />
-            </Item>
-          </Grid>
-          <Grid item xs={12}>
-            <Item>
-              <ServicesSection />
-            </Item>
-          </Grid>
-          <Grid item xs={12}>
-            <Item>
-              <ContactSection />
-            </Item>
-          </Grid>
-        </Grid>
-        <CircularButton onClick={togglePopUp} />
-        {showPopUp && <PopUp closePopup={togglePopUp} />}
-      </OuterContainer>
-    </Box>
+          <CircularButton onClick={togglePopUp} />
+          {showPopUp && <PopUp closePopup={togglePopUp} />}
+        </OuterContainer>
+      </Box>
+    </Container>
   );
 };
 
